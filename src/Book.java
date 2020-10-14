55e5721e44156b1303bfb587ef5c15d204eab841
= <=>
2020-10-14 20:10:45 +0330

    some changes in book's method and fields

--- src/Book.java
+++ src/Book.java
@@ -3,12 +3,15 @@ import java.util.Set;
 
 public class Book
 {
+    private static long firstId = 1000;
+
     private String name;
     private Set<Author> authors;
     private Set<Publisher> publishers;
     private double score;
     private Genre genre;
     private int availableNumber;
+    private long id;
 
     public Book(String name, Genre genre, int publisherScore, int availableNumber)
     {
@@ -18,5 +21,19 @@ public class Book
         this.publishers = new HashSet<>();
         this.score = publisherScore;
         this.availableNumber = availableNumber;
+        this.id = firstId++;
+    }
+
+    public boolean isAvailable()
+    {
+        return this.availableNumber > 0;
+    }
+    public void lowerAvailableNumbers()
+    {
+        this.availableNumber -= 1;
+    }
+    public void addAvailableNumbers()
+    {
+        this.availableNumber += 1;
     }
 }
--- src/OrdinaryUser.java
+++ src/OrdinaryUser.java
@@ -1,18 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class OrdinaryUser implements AbleToBorrow{
     private int score;
-    private int
+    private boolean isTrustWorthy;
+    private Set<Book> borrowedBooks = new HashSet<>();
+
+    public OrdinaryUser()
+    {
+        this.score = 0;
+        this.isTrustWorthy = false;
+    }
+
     @Override
-    public void borrow(Book book) {
+    public void borrow(Book book)
+    {
+        if(book.isAvailable())
+        {
+            if(this.hasRightToBorrow())
+            {
+                book.lowerAvailableNumbers();
+                borrowedBooks.add(book);
+            }
+        }
 
     }
 
     @Override
     public boolean hasRightToBorrow() {
-        return false;
+        return borrowedBooks.size() < 5;
     }
 
     @Override
-    public void returnBook(Book book) {
-
+    public void returnBook(Book book)
+    {
+        borrowedBooks.remove(book);
+        book.addAvailableNumbers();
     }
 }
