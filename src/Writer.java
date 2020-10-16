5c965d6ec3463f6a3a5d12d175d4fed5bdadac95
= <=>
2020-10-16 15:51:33 +0330

    added method Writer.writeBookContextInFile so we can easily access these books!

--- src/Writer.java
+++ src/Writer.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -29,18 +32,29 @@ public class Writer extends SpecialPerson {
             System.out.println("write more books!");
         }
     }
+    private void writeBookContextInFile(String name, Scanner scanner) throws IOException {
+        FileWriter bookFile = new FileWriter(name + " .txt");
+        System.out.println("enter your book dates from PDF!");
+        StringBuilder context = new StringBuilder();
+        while (scanner.hasNextLine())
+            context.append(scanner.nextLine());
+        bookFile.write(context.toString());
+
+    }
 
     private boolean isEligible() {
         return this.score >= 200;
     }
-    public void writeBooks()
-    {
+    public void writeBooks() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("please write your book name");
         String name = scanner.nextLine();
         System.out.println("good, now please select your book genre");
         String genreIn = scanner.next();
         Genre genre = Genre.valueOf(genreIn);
+        /* we will be able to save the book into file */
+        writeBookContextInFile(name, scanner);
+
         System.out.println("great, now please introduce your book");
         String explain = scanner.nextLine();
         Book book = new Book(name, explain, genre, this);
@@ -58,4 +72,5 @@ public class Writer extends SpecialPerson {
     {
         return this.isBestSeller;
     }
+
 }
