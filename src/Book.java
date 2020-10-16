c260a8a1a824771954356cd1fe3f575cfbd18b2c
= <=>
2020-10-16 11:19:30 +0330

    initial commit on Writer.java & development of Book.java added some methods !

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,67 @@
+import java.util.*;
+
 public class Book
 {
+    private String name;
+    private Genre genre;
+    private Set<Writer> writers;
+    private Set<Publisher> publishers;
+    private int personWhoBorrow;
+    private int numberOfPublish;
+    private boolean isPublished;
+    private String explanation;
+    private Map<String, Integer> reviews;
 
+    public Book(String name, String explanation, Genre genre, Writer writer)
+    {
+        this.name = name;
+        this.explanation = explanation;
+        this.genre = genre;
+        this.writers = new HashSet<>();
+        this.writers.add(writer);
+        isPublished = false;
+        System.out.println("now your book is ready");
+        System.out.println("send it to a publisher!");
+    }
+    public Book(String name,String explanation, Genre genre, Publisher publisher, int numberOfPublish, Writer... writers)
+    {
+        this(name, explanation, genre, writers[0]);
+        for (Writer w: writers)
+            addWriter(w);
+        this.isPublished = true;
+        this.numberOfPublish = numberOfPublish;
+        this.publishers = new HashSet<>();
+        this.publishers.add(publisher);
+
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        return o instanceof Book && ((Book) o).name.equals(name);
+    }
+    public void addWriter(Writer writer)
+    {
+        writers.add(writer);
+    }
+    public void addPublisher(Publisher publisher, int numberOfPublish)
+    {
+        this.publishers.add(publisher);
+        this.numberOfPublish += numberOfPublish;
+    }
+    public void addPersonToBorrow()
+    {
+        if(this.isAvailable())
+        {
+            this.personWhoBorrow += 1;
+        }
+        else
+        {
+            System.out.println("the book is not available now!");
+        }
+    }
+
+    private boolean isAvailable()
+    {
+        return numberOfPublish >= personWhoBorrow;
+    }
 }
--- src/Writer.java
+++ src/Writer.java
@@ -1,3 +1,54 @@
-public class Writer {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class Writer extends SpecialPerson {
+    private int score;
+    private int salary;
+    private boolean isBestSeller;
+    private Set<Book> finishedBooks;
+    private Set<Book> publisherSentBooks;
+    public Writer(String name, String lastName, long id, String username, String password)
+    {
+        super(name, lastName, id, username, password);
+        this.score = 0;
+        this.salary = 0;
+        this.isBestSeller = false;
+        this.finishedBooks = new HashSet<>();
+        this.publisherSentBooks = new HashSet<>();
+    }
+    public void makeBestSeller()
+    {
+        if(this.isEligible())
+        {
+            this.salary = 100;
+        }
+        else
+        {
+            System.out.println("you are not eligible");
+            System.out.println("write more books!");
+        }
+    }
+
+    private boolean isEligible() {
+        return this.score >= 200;
+    }
+    public void writeBooks()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please write your book name");
+        String name = scanner.nextLine();
+        System.out.println("good, now please select your book genre");
+        String genreIn = scanner.next();
+        Genre genre = Genre.valueOf(genreIn);
+        System.out.println("great, now please introduce your book");
+        String explain = scanner.nextLine();
+        Book book = new Book(name, explain, genre, this);
+        finishedBooks.add(book);
+        System.out.println("great, now you can send your book to publisher at any time");
+    }
+    public void sendToPublisher()
+    {
+
+    }
 }
